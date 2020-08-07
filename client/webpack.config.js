4e164eda5d89038f5994ba85bbc472111a9ad9ec
Sean Johnson <Sean.Johnson@runbeck.net>
2020-08-07 11:14:03 -0700

    Added client build.

---
+++ client/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.tsx'
+    },
+    output: {
+        filename: '[name]-[hash:6].js',
+        path: path.resolve(__dirname, './build')
+    },
+    optimization: {
+        usedExports: true
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader'
+        }, {
+            test: /\.js$/,
+            enforce: 'pre',
+            use: ['source-map-loader'],
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        host: '0.0.0.0',
+        port: 8080,
+        historyApiFallback: true
+    }
+};
