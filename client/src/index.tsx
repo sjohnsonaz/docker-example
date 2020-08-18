275ce727975892a53606b282be66bb4e3a24683f
Sean Johnson <sjohnson@sjohnsonaz.com>
2020-08-18 15:53:43 -0700

    Added reverse proxy.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,8 @@
-window.onload = function () {
+window.onload = async function () {
     console.log('test');
+    const root = document.getElementById('root');
+    if (root) {
+        const data = await (await fetch('/api')).text();
+        root.innerHTML = data;
+    }
 }
