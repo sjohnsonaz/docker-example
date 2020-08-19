1e25193535d20e0a99d3bb3e62874adab2482c84
Sean Johnson <sjohnson@sjohnsonaz.com>
2020-08-19 12:50:37 -0700

    Optimized Server.start method.

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -3,8 +3,23 @@ import http from 'http';
 export class Server extends http.Server {
     async start(port: number) {
         return new Promise<void>((resolve, reject) => {
-            this.on('error', reject);
-            this.on('listening', resolve)
+            const startup = () => {
+                this.on('error', onError);
+                this.on('listening', onListening)
+            };
+            const cleanup = () => {
+                this.off('error', onError);
+                this.off('listening', onListening);
+            };
+            const onError = (err: Error) => {
+                reject(err);
+                cleanup();
+            };
+            const onListening = () => {
+                resolve();
+                cleanup()
+            };
+            startup();
             this.listen(port);
         });
     }
